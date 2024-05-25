80331ba882292f8a6eeb2f08e6dd2b7dcffd832b
Aboneh5 <aboneh.teshome55@gmail.com>
2024-05-25 10:54:16 +0300

    Update register.js

--- src/register.js
+++ src/register.js
@@ -15,7 +15,7 @@ const Register = () => {
 
     const configuration = {
       method: "post",
-      url: "http://localhost:3001/register",
+      url: "https://test-back-2-wnv8.onrender.com/register",
       data: { email, password },
     };
     axios(configuration);
